7250b2648b56e1b48cd986235cea304ba8ebd230
AhmedYaser.Mt <ahmed.yaser.mt@gmail.com>
2026-02-11 17:59:11 +0400

    Extensions [ 1 to 7 ]

--- Project 16 [ Queue Class ].cpp
+++ Project 16 [ Queue Class ].cpp
@@ -8,11 +8,6 @@ int main()
     clsMyQueue <short> MyQueue;
 
     MyQueue.Push(10);
-    MyQueue.Pop();
-
-    cout << MyQueue.Front() << endl;
-
-    /*MyQueue.Push(10);
     MyQueue.Push(20);
     MyQueue.Push(30);
     MyQueue.Push(40);
@@ -26,9 +21,34 @@ int main()
     cout << "\nQueue Back  : " << MyQueue.Back() << endl;
 
     MyQueue.Pop();
-
     cout << "\n\nQueue after pop() :\n";
-    MyQueue.Print();*/
+    MyQueue.Print();
+
+    cout << "\n\nItem (2) : " << MyQueue.GetItem(2);
+
+    MyQueue.Reverse();
+    cout << "\n\nQueue after Reverse() :\n";
+    MyQueue.Print();
+
+    MyQueue.UpdateItem(2, 600);
+    cout << "\n\nQueue after updating Item(2) to 600 :\n";
+    MyQueue.Print();
+
+    MyQueue.InsertAfter(2, 800);
+    cout << "\n\nQueue after Inserting 800 after Item(2) :\n";
+    MyQueue.Print();
+
+    MyQueue.InsertAtFront(1000);
+    cout << "\n\nQueue after Inserting 1000 at Front :\n";
+    MyQueue.Print();
+
+    MyQueue.InsertAtBack(2000);
+    cout << "\n\nQueue after Inserting 2000 at Back :\n";
+    MyQueue.Print();
+
+    MyQueue.Clear();
+    cout << "\n\Queue after Clear() :\n";
+    MyQueue.Print();
 
     system("pause>0");
     return 0;
--- clsMyQueue.h
+++ clsMyQueue.h
@@ -10,23 +10,17 @@ class clsMyQueue
 protected:
 
 	clsDblLinkedList <T> _MyList;
-	T _Back = NULL;
 
 public:
 
 	void Push(T Item)
 	{
 		_MyList.InsertAtEnd(Item);
-
-		_Back = Item;
 	}
 
 	void Pop()
 	{
 		_MyList.DeleteFirstNode();
-
-		if (_MyList.IsEmpty())
-			_Back = NULL;
 	}
 
 	void Print()
@@ -46,15 +40,49 @@ public:
 
 	T Front()
 	{
-		if (IsEmpty())
-			return NULL;
-		else
-			return _MyList.Head->Value;
+		return _MyList.GetItem(0);
 	}
 
 	T Back()
 	{
-		return _Back;
+		return _MyList.GetItem(Size() - 1);
+	}
+
+	//----------------------------------------------------
+
+	T GetItem(int Index)
+	{
+		return _MyList.GetItem(Index);
+	}
+
+	void Reverse()
+	{
+		_MyList.Reverse();
+	}
+
+	void UpdateItem(int Index, T NewValue)
+	{
+		_MyList.UpdateItem(Index, NewValue);
+	}
+
+	void InsertAfter(int Index, T NewValue)
+	{
+		_MyList.InsertAfter(Index, NewValue);
+	}
+
+	void InsertAtFront(T Value)
+	{
+		_MyList.InsertAtBeginning(Value);
+	}
+
+	void InsertAtBack(T Value)
+	{
+		_MyList.InsertAtEnd(Value);
+	}
+
+	void Clear()
+	{
+		_MyList.Clear();
 	}
 };
 
